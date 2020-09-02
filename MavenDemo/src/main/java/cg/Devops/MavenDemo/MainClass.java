80d8adb8b9fecb0a9e67f7f2d7a902d15a83bd1b
Inturi <ravi-venkatesh.inturi@capgemini.com>
2020-09-02 11:36:26 +0530

    MavenDemo

---
+++ MavenDemo/src/main/java/cg/Devops/MavenDemo/MainClass.java
@@ -0,0 +1,11 @@
+package cg.Devops.MavenDemo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hai maven project");
+
+	}
+
+}
---
+++ MavenDemo/src/test/java/cg/Devops/MavenDemo/MainTest.java
@@ -0,0 +1,28 @@
+package cg.Devops.MavenDemo;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+	
+	@Test
+	public void test1() {
+		
+	}
+	
+	@Test
+	public void test2() {
+		
+	}
+	
+	@Test
+	public void test3() {
+		Assert.fail("test 3 failed");
+		
+	}
+	
+	
+	
+
+	
+
+}
